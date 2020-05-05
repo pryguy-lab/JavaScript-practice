376933c283df2293c50c731225f2578962d1687a
Mike Pry <mikepry@hotmail.com>
2020-05-05 13:18:24 -0400

    more javaScript training

---
+++ 02basics/learnifElse.js
@@ -0,0 +1,10 @@
+if (false){
+    console.log('i am inside if block')
+    console.log('i am still inside if block')
+}
+else if (true){
+    console.log('I AM INSIDE IF IN CAPS BLOCK')
+}
+else{
+    console.log('NOTICE THIS PART')
+}
